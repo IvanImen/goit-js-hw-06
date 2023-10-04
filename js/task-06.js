23aaff5b5ab69ec622ff7594bcd75dff3175d938
IvanImen <134809500+IvanImen@users.noreply.github.com>
2023-10-05 00:15:00 +0300

    task06

---
+++ js/task-06.js
@@ -0,0 +1,18 @@
+const input = document.querySelector("#validation-input");
+
+input.addEventListener("blur", textLenght);
+
+console.dir(input);
+
+function textLenght() {
+    const expectedLength = parseInt(input.getAttribute('data-length'));
+    const currentLength = input.value.length;
+
+    if (currentLength === expectedLength) {
+      input.classList.remove('invalid');
+      input.classList.add('valid');
+    } else {
+      input.classList.remove('valid');
+      input.classList.add('invalid');
+    }
+}
